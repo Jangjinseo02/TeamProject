d426b4df3a12278c834337ef9b38c7a1c032ed39
Jangjinseo <jsu_02@naver.com>
2023-06-07 13:39:07 +0900

    UnityEditor.EditorApplication.ExitPlaymode(); 에러 발생 및 빌드 실패가 되어 사용하지 않도록 수정해두었습니다. 확인 부탁드립니다.

--- Assets/TeamProject/Scripts/UI/DialogTest.cs
+++ Assets/TeamProject/Scripts/UI/DialogTest.cs
@@ -40,7 +40,7 @@ public class DialogTest : MonoBehaviour
 
         yield return new WaitForSeconds(2);
 
-        UnityEditor.EditorApplication.ExitPlaymode();
+        //UnityEditor.EditorApplication.ExitPlaymode();
     }
 }
 
